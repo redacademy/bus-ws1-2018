35625e4acb9edd59865b194d298b2066c0870e73
Nasma <nasma.mlamali@icloud.com>
2018-06-27 19:16:21 -0700

    products page custom field suite loop

---
+++ themes/walkingbus/template-parts/content-products.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Template part for displaying single posts.
+ *
+ * @package inhabitent
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php 
+	if ( has_post_thumbnail() ) : ?>
+			<?php 
+			the_post_thumbnail( 'large' ); ?>
+		<?php
+		 endif; ?>
+
+	
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php red_starter_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
+
