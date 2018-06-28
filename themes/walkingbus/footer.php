e45305ea6e9055fec627d2e5ee1ca394525b7c52
Igor <igorcmnog@gmail.com>
2018-06-27 21:07:24 -0700

    footer done

--- themes/walkingbus/footer.php
+++ themes/walkingbus/footer.php
@@ -11,7 +11,46 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+				<section class="main-container-footer">
+					<div class="footer-links">
+						<div class="featuredon-links"><p>featured-on</p>
+						<img src="assets/images/featured-logo.svg">
+						</div>
+						<div class="followuson-links"><p>follow us on</p>
+						<ul>
+							<li><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+							<li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>	
+							<li><a href="https://plus.google.com/"><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+						</div>
+					</div>
+					<section class="subscription-container">
+						<p>subscribe <span>to our </span>newsletter</p>
+						<form><input placeholder="Email Adress"></input></form>
+						<button>Sign Up</button>
+						<div class="followuson-links-mob">
+						<ul>
+							<li><a href="https://www.facebook.com/"><i class="fab fa-facebook-f"></i></a></li>
+							<li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
+							<li><a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a></li>
+							<li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>	
+							<li><a href="https://plus.google.com/"><i class="fab fa-google-plus-g"></i></a></li>
+							<li><a href="https://www.linkedin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+						</ul>
+						</div>
+						<p id="contactus">contact us</p>
+					</section>
+						<div class="contact-info">
+						<div class="contact"><p>700 West 16th Avenue, Vancouver, BC, V5Z 1S7</p>
+						<p>E: info@thewalkingschoolbus.com</p>
+						<p>T: (778) 989-4363</p></div>
+						<div class="registration"><p class="registration-footer">Charitable Registration # 81033 8723 RR0001</p>
+						<p class="registration-footer">2018 The Walking School Bus, All rights reserved</p></div>
+						</div>
+					</section>
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- themes/walkingbus/header.php
+++ themes/walkingbus/header.php
@@ -6,12 +6,13 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
 
 	<?php wp_head(); ?>
 	</head>
