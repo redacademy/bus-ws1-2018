1b9248a776e684c78ee45f794df24d85114ec278
Nasma <nasma.mlamali@icloud.com>
2018-06-28 20:59:36 -0700

    shop page styling

--- themes/walkingbus/archive-products.php
+++ themes/walkingbus/archive-products.php
@@ -15,48 +15,64 @@ Template Name: products
 
 <?php get_header();  ?>
 
-    <div class = "shop-menu">
+<div class = "site-content-wrapper">
 
-        <h1> <?php the_title(); ?> </h1>
+<section class = "shop-page-header">
 
-    
-        <nav class = "categories-menu"> 
+    <div class = "shop-info">
+       
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-            <!-- loop to display the links to the shopping categories  -->
-
-            <?php 
-                // $terms = get_terms('Products');
-                // echo '<ul class = "product-type">';
-                // foreach ($terms as $term){
-                //     echo '<li class="type"><a href="'.get_term_link($term).'">'.$term->name.'</a></li>' ;
-                // }  
-                // echo '</ul>';
-            ?>
-
-        </nav> <!-- .categories-menu-->            
-
-    </div> <!-- .shop-menu -->
+          <?php the_content();?>
+            
+        
+        <?php endwhile;?>
+        <?php endif; ?>
+    </div>
+       
 
 
+    <div class = "shop-banner">
 
-    <div class = "products-gallery">
+        <!-- loop to display the banner  -->
 
-        <!-- loop to display all the products -->
+        <div class= "banner">
+
+            <?php  $images = CFS()->get('shop_banner'); 
+                        foreach ($images as $image) {
+                            echo '<img src="'.$image['banner'].'"/>';
+                        
+            }?>
+
+            <h1> <?php the_title(); ?> </h1>
+
+        </div>
+    </div>
+
+</section>
+
+<section class = "products-gallery">
+
+    <div class = "products-gallery-container">
+
+    <!-- loop to display all the products, to change the number of products displayed 
+        set the post per page number to the desired value -->
+
 			
         <?php
-            $args = array( 'post_type' => 'products', 'order' => 'ASC', 'posts_per_page' => -1  );
-            $products = new WP_Query( $args ); // instantiate our object
+            $args = array( 'post_type' => 'products', 'order' => 'ASC', 'posts_per_page' => 4  );
+            $products = new WP_Query( $args ); 
         ?>
 
             <?php if ( $products->have_posts() ) : ?>
             <?php while ( $products->have_posts() ) : $products->the_post(); ?>
 
 
-      
-
         <div class = "single-product">
 
             <div class = "product-image"> 
+
+        <!-- custom field loop to display the products thumbnails as a gallery-->
 
                 <?php  $images = CFS()->get('products');
 				   foreach ($images as $image) {
@@ -67,8 +83,11 @@ Template Name: products
 
             <div class = "product-info">
 
+            <!-- call custom field suite for product name and price-->
+
                 <p> <?php the_title();?> </p>
                 <p> <?php echo CFS()->get( 'price' ); ?>  </p>
+          
 
             </div> <!-- .product-info-->
 
@@ -79,10 +98,12 @@ Template Name: products
 
             <?php endwhile; ?>
 
-    </div> <!-- products-gallery -->
+    </div> <!-- products-gallery-container -->
+    </section>
+    </div>
     
     
-            <?php wp_reset_postdata(); ?>
+            <?php wp_reset_postdata(); ?> 
 
             <?php else : ?>
 
