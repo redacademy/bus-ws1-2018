f054200a8505ebb7f58cd1bbf295c5ebdd7b4d2c
Igor <igorcmnog@gmail.com>
2018-06-30 16:41:30 -0700

    widget for footer done

--- themes/walkingbus/functions.php
+++ themes/walkingbus/functions.php
@@ -64,6 +64,16 @@ function red_starter_widgets_init() {
 		'before_title'  => '<h2 class="widget-title">',
 		'after_title'   => '</h2>',
 	) );
+
+	register_sidebar( array(
+		'name' => 'Footer',
+		'id' => 'footer-info',
+		'description' => 'Appears in the footer area',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget' => '</aside>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+		) );
 }
 add_action( 'widgets_init', 'red_starter_widgets_init' );
 
