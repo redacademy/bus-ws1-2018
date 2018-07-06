59c7bac90080a8fe51a30dd0ebf0292ff59bfa36
Igor <igorcmnog@gmail.com>
2018-07-05 20:52:06 -0700

    Impact page for mobile - working on

--- themes/walkingbus/page-impact.php
+++ themes/walkingbus/page-impact.php
@@ -40,11 +40,13 @@ Template Name: impact
             <span class="dot" onclick="currentSlide(2)"></span> 
             <span class="dot" onclick="currentSlide(3)"></span> 
             </div>
-
-        <section class="see-reports">
-            <p>See how we are tracking in each region by reviewing our annual report for 2016 - 17.</p><br>
-            <p>For old reports, go here</p>
-        </section>
+        
+        <div class="see-reports-wrapper">
+            <section class="see-reports">
+                <p>See how we are tracking in each region by reviewing our annual report for 2016 - 17.</p><br>
+                <p>For old reports, go here</p>
+            </section>
+        </div>
 
         <section class="videos-impact">
             <embed width="420" height="315"src="https://www.youtube.com/embed/tgbNymZ7vqY"> 
@@ -58,26 +60,41 @@ Template Name: impact
                     $args = array( 
                         'post_type' => 'news',
                         'posts_per_page' => 7, 
+                        'order' => 'ASC',
                     ); 
                     $news = new WP_Query( $args );
                 ?>
                 <?php if ( $news->have_posts() ) : ?>
-                <?php while ( $news->have_posts() ) : $news->the_post(); ?>
-                    <div class = "new-impact"  style="background-image: url(<?php the_post_thumbnail_url(); ?>)"><a href="<?php the_permalink(); ?>">
-                        <div class="text-area-news">
-                        <h2><a class="title-area-news" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                        <a class="arrow-link-news" href="<?php the_permalink(); ?>">></a></h2>
+                    <?php while ( $news->have_posts() ) : $news->the_post(); ?>
+                        
+                        <?php if (has_post_thumbnail( $post->ID ) ): ?>
+                        <div class = "new-impact"  style="background-image: url(<?php the_post_thumbnail_url(); ?>)"><a href="<?php the_permalink(); ?>">
+                            <div class="text-area-news">
+                            <h2><a class="title-area-news" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                            <a class="arrow-link-news" href="<?php the_permalink(); ?>">></a></h2>
+                            </div>
+                        </a></div>
+                        <?php else : ?>
+                        <br>
+                        <div class="news-nophoto-wrapper">
+                            <div class="news-nophoto-area">
+                                <a class="news-nophoto" href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                <a class="arrow-link-news-nophoto" href="<?php the_permalink(); ?>">></a></h2>
+                            </div>
                         </div>
-                    </a></div>
+
+                        <?php endif; ?>
+
                     <?php endwhile; ?>
+                    
+                    <div class="load-more-area">
+                        <a class="load-more">Load more</a>
+                    </div>
+                    
                     <?php wp_reset_postdata(); ?>
                     
-                    <?php else : ?>
-                    <?php while ( $news->have_posts() ) : $news->the_post(); ?>
-                        <a class="title-area-news" href="<?php the_permalink(); ?>"><?php the_title(); ?></a> 
-                    <?php endwhile; ?>
-
-                    <h2>Nothing found!</h2>
+                <?php else : ?>
+                <h2>Nothing found!</h2>
                 <?php endif; ?>
             </div>
 
