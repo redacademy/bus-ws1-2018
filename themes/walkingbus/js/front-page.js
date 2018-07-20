dd69ce44daf85701f7ea5456f9aae11f06428a28
Rafael Sliveira Cordeiro <rafaelsc@rafaelsc.net>
2018-07-19 20:23:31 -0700

    Update Front Page

---
+++ themes/walkingbus/js/front-page.js
@@ -0,0 +1,40 @@
+var $j = jQuery.noConflict();
+
+$j(function() {
+
+    var isMovingTheBus = false;
+    var moveTheBusToProgressFunction = function() {
+        $j('#myProgress').off('appear', moveTheBusToProgressFunction);
+        if(isMovingTheBus) {
+            return;
+        }
+        isMovingTheBus = true;
+
+        var elem = document.getElementById('progress-bar'); 
+        if(!elem) {
+            return;
+        }
+
+        var width = 1;
+        var endProgress = elem.getAttribute('data-progress');
+
+        setTimeout(function(){
+            var id = setInterval(frame, 16);
+            function frame() {
+                if (width >= endProgress) {
+                    $j('#progress-bar-text').show(200);
+                    clearInterval(id);
+                } else {
+                    width++; 
+                    elem.style.width = width + '%'; 
+                }
+            }
+        }, 1000);
+    }
+
+    $j.appear('#myProgress');
+    $j('#myProgress').on('appear', moveTheBusToProgressFunction);
+
+    // $j('.infographic-container').slick();
+
+});
