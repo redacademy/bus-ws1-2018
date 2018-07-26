7d7351aaf0f0cea37df52fbaea8b465f28e41ee8
Alberto <jalberto.robles@outlook.com>
2018-07-25 22:24:25 -0700

    merging conflicts

--- themes/walkingbus/js/general.js
+++ themes/walkingbus/js/general.js
@@ -147,6 +147,8 @@ $j(function() {
         mobileFirst: true
 
       });
+    });
+
 });
 
 $j(function() {
@@ -206,14 +208,13 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1} 
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none"; 
-  }
-  slides[slideIndex-1].style.display = "block"; 
-  dots[slideIndex-1].className += " active";
-}
-});
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block"; 
+   }
+
