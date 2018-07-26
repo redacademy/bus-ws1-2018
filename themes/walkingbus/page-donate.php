0ae89ae2ca2cd63ad8321e7446807bba3e015361
Igor <igorcmnog@gmail.com>
2018-07-26 14:15:05 -0700

    footer style changed

--- themes/walkingbus/page-donate.php
+++ themes/walkingbus/page-donate.php
@@ -9,10 +9,10 @@ Template Name: donate
 
 	<div id="primary" class="content-area-donate">
 		<main id="main" class="site-main-donate" role="main">
-            <div class="donation-image image-page-donate">
+            <div class="donation-image image-page-donate" style="background-image: url(<?php echo CFS()->get('donation_image'); ?>)">    
                 <a href="" class="donate-btn">donation</a>
             </div>
-            <div class="sponsor-image image-page-donate">
+            <div class="sponsor-image image-page-donate" style="background-image: url(<?php echo CFS()->get('sponsor_image'); ?>)">
                 <a class="donate-btn" href="http://bus.van.cp.academy.red/scholarships/">sponsor scholar</a>
             </div>
         </main><!-- #main -->
