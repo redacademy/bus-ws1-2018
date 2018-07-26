890b2c6c5baec0f06cb6a175475381163189654c
Nasma <nasma.mlamali@icloud.com>
2018-07-26 11:28:32 -0700

    styling

--- themes/walkingbus/archive-products.php
+++ themes/walkingbus/archive-products.php
@@ -16,11 +16,9 @@ Template Name: products
             <div class = "shop-info">
             
                 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-              
                 
-                <?php// endwhile;?>
-                <?php// endif; ?>
+                <?php endwhile;?>
+                <?php endif; ?>
             </div> <!-- shop-info-->
         
 
@@ -45,7 +43,7 @@ Template Name: products
                 </div> <!-- BANNER-->
 
             </div><!-- SHOP BANNER-->
-
+      
         </section> <!-- shop-page-header -->
 
 <section class = "products-gallery">
@@ -58,20 +56,20 @@ Template Name: products
 	  <?php the_content();?>
                     
         <?php
-           // $args = array( 'post_type' => 'products', 'order' => 'ASC', 'posts_per_page' => -1 );
-          //  $products = new WP_Query( $args ); 
+           $args = array( 'post_type' => 'products', 'order' => 'ASC', 'posts_per_page' => -1 );
+          $products = new WP_Query( $args ); 
         ?>
 
-            <?php // if ( $products->have_posts() ) : ?>
-            <?php // while ( $products->have_posts() ) : $products->the_post(); ?>
+            <?php  if ( $products->have_posts() ) : ?>
+            <?php  while ( $products->have_posts() ) : $products->the_post(); ?>
 
 
         <div class = "single-product">
 
             <div class = "product-image"> 
-
+ 
                 <picture>
-                    <?php // the_post_thumbnail();?>
+                    <?php  the_post_thumbnail();?>
                 </picture>
       
              </div>  <!-- .product-image -->
@@ -81,11 +79,9 @@ Template Name: products
 
              <!-- call custom field suite for product name and price-->
 
-                <p> <a href = "<?php // the_permalink();?>"> <?php// the_title();?>  </a></p>
-                <p> <?php // echo CFS()->get( 'price' ); ?>  </p>
-                <p> <?php //echo CFS()->get( 'button' ); ?> </p>
-                <p> <?php //echo CFS()->get( 'load_more' ); ?> </p>
-          
+                <p> <a href = "<?php  the_permalink();?>"> <?php// the_title();?>  </a></p>
+                <p> <?php  echo CFS()->get( 'price' ); ?>  </p>
+               
             </div> <!-- .product-info-->
 
 
